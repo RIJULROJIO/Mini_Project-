e6a78fbb41bb89ddb71ff5af9109d200184e4eb3
RIJULROJIO <rijulrojio@gmail.com>
2023-12-07 22:54:51 +0530

    MINI PROJECT FINISHED

--- Mini Project Code/property_rental_platform/rental/tests.py
+++ Mini Project Code/property_rental_platform/rental/tests.py
@@ -1,56 +1,189 @@
-from datetime import datetime
-from django.test import TestCase
-from selenium import webdriver
 
-from selenium.webdriver.common.keys import Keys
+
+
+# # import time
+# # import unittest
+# # from selenium import webdriver
+# # from selenium.webdriver.common.by import By
+
+# # class LoginTest(unittest.TestCase):
+
+# #     def setUp(self):
+# #         self.driver = webdriver.Chrome()
+
+# #     def test_successful_login(self):
+# #         self.driver.get('http://127.0.0.1:8000/login')
+# #         username_input = self.driver.find_element(By.ID, 'username')
+# #         password_input = self.driver.find_element(By.ID, 'password')
+
+# #         # Replace 'valid_username' and 'valid_password' with actual credentials
+# #         username_input.send_keys('rijulrojio')
+# #         password_input.send_keys('rR@12345')
+
+# #         login_button = self.driver.find_element(By.ID, 'login')
+# #         login_button.click()
+
+# #         time.sleep(2)  # Adjust the time as needed
+
+# #         # ... (Your existing login code)
+
+# #         # Check if redirected to the home page
+# #         self.assertIn('http://127.0.0.1:8000/ownerpage', self.driver.current_url.lower())
+
+# #         # Click on the "Dashboard" link
+# #         dashboard_link = self.driver.find_element(By.LINK_TEXT, 'Dashboard')
+# #         dashboard_link.click()
+
+# #         # Check if redirected to the owner's dashboard
+# #         self.assertIn('http://127.0.0.1:8000/ownerpg', self.driver.current_url.lower())
+
+# #         # Click on the "Add Properties" link
+# #         add_properties_link = self.driver.find_element(By.LINK_TEXT, 'Add Properties')
+# #         add_properties_link.click()
+
+# #         # Fill the property details in the form
+# #         property_type_dropdown = self.driver.find_element(By.ID, 'property_type')
+# #         address_input = self.driver.find_element(By.ID, 'address')
+# #         monthly_rent_input = self.driver.find_element(By.ID, 'monthly_rent')
+# #         security_deposit_input = self.driver.find_element(By.ID, 'security_deposit')
+# #         lease_duration_dropdown = self.driver.find_element(By.ID, 'lease_duration')
+# #         availability_date_input = self.driver.find_element(By.ID, 'availability_date')
+
+# #         # Replace these values with actual property details
+# #         property_type_dropdown.send_keys('Apartment')
+# #         address_input.send_keys('123 Main St')
+# #         monthly_rent_input.send_keys('1500')
+# #         security_deposit_input.send_keys('2000')
+# #         lease_duration_dropdown.send_keys('1 year')
+# #         availability_date_input.send_keys('01-02-2022')
+
+# #         # Submit the form
+# #         submit_button = self.driver.find_element(By.CSS_SELECTOR, '#add-property-form button[type="submit"]')
+# #         submit_button.click()
+
+# #         time.sleep(2)  # Adjust the time as needed
+
+# #         # Now you can add assertions to check if the property was added successfully
+# #         # For example, check for success messages or check if the property appears in the list
+
+# #     def tearDown(self):
+# #         self.driver.quit()
+
+# # if __name__ == "__main__":
+# #     unittest.main()
+
+
+# import time
+# import unittest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+
+# class LoginTest(unittest.TestCase):
+
+#     def setUp(self):
+#         self.driver = webdriver.Chrome()
+
+#     def test_successful_login(self):
+#         self.driver.get('http://127.0.0.1:8000/login')
+#         username_input = self.driver.find_element(By.ID, 'username')
+#         password_input = self.driver.find_element(By.ID, 'password')
+
+#         # Replace 'valid_username' and 'valid_password' with actual credentials
+#         username_input.send_keys('jubil')
+#         password_input.send_keys('jJ@12345')
+
+#         login_button = self.driver.find_element(By.ID, 'login')
+#         login_button.click()
+
+#         time.sleep(2)  # Adjust the time as needed
+
+
+#         # Check if redirected to the tenantpage
+#         self.assertIn('http://127.0.0.1:8000/tenantpage', self.driver.current_url.lower())
+
+#         # Click on the "Dashboard" link
+#         dashboard_link = self.driver.find_element(By.LINK_TEXT, 'Dashboard')
+#         dashboard_link.click()
+
+#         time.sleep(2)  # Adjust the time as needed
+
+#         self.assertIn('http://127.0.0.1:8000/tenantpg', self.driver.current_url.lower())
+#         property_type_dropdown = self.driver.find_element(By.ID, 'property_type')
+#         min_rent_input = self.driver.find_element(By.NAME, 'min_rent')
+#         max_rent_input = self.driver.find_element(By.NAME, 'max_rent')
+#         search_button = self.driver.find_element(By.CSS_SELECTOR, '#property-search-form button[type="submit"]')
+
+#         # Replace these values with the desired search criteria
+#         property_type_dropdown.send_keys('Apartment')
+#         min_rent_input.send_keys('1000')
+#         max_rent_input.send_keys('2000')
+#         search_button.click()
+
+#         time.sleep(2)  # Adjust the time as needed
+
+
+#     def tearDown(self):
+#         self.driver.quit()
+
+# if __name__ == "__main__":
+#     unittest.main()
+
 import time
+import unittest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-class Hosttest(TestCase):
+class LoginTest(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(10)
-        self.live_server_url = 'http://127.0.0.1:8000/'
+
+    def test_successful_login(self):
+        self.driver.get('http://127.0.0.1:8000/login')
+        username_input = self.driver.find_element(By.ID, 'username')
+        password_input = self.driver.find_element(By.ID, 'password')
+
+        # Replace 'valid_username' and 'valid_password' with actual credentials
+        username_input.send_keys('rijulrojio')
+        password_input.send_keys('rR@12345')
+
+        login_button = self.driver.find_element(By.ID, 'login')
+        login_button.click()
+
+        time.sleep(2)  # Adjust the time as needed
+
+        # Check if redirected to the ownerpage
+        self.assertIn('http://127.0.0.1:8000/ownerpage', self.driver.current_url.lower())
+
+        # Click on the "Dashboard" link
+        dashboard_link = self.driver.find_element(By.LINK_TEXT, 'Dashboard')
+        dashboard_link.click()
+
+        time.sleep(2)  # Adjust the time as needed
+
+        # Check if redirected to the ownerpg
+        self.assertIn('http://127.0.0.1:8000/ownerpg', self.driver.current_url.lower())
+
+        # Click on the "Manage Properties" link
+        manage_properties_link = self.driver.find_element(By.LINK_TEXT, 'Manage Properties')
+        manage_properties_link.click()
+
+        time.sleep(2)  # Adjust the time as needed
+
+        # Check if redirected to the manageprop
+        self.assertIn('http://127.0.0.1:8000/manageprop', self.driver.current_url.lower())
+
+        # Click on the "Accept Request" button
+        accept_request_button = self.driver.find_element(By.CSS_SELECTOR, 'form button.btn-primary')
+        accept_request_button.click()
+
+        time.sleep(2)  # Adjust the time as needed
+
+        # Now you can add assertions to check if the acceptance was successful
+        # For example, check for success messages or changes in the UI
 
     def tearDown(self):
         self.driver.quit()
 
-    def test_01_login_page(self):
-        driver = self.driver
-        driver.get(self.live_server_url)
-        driver.maximize_window()
-        time.sleep(1)
-
-        # Click on the login icon
-        logd = driver.find_element(By.CSS_SELECTOR, "i#log-drop.bi.bi-person-gear.customSize.mr-3")
-        logd.click()
-        time.sleep(1)
-
-        # Click on the login link
-        login = driver.find_element(By.CSS_SELECTOR, "a[href='/login/']")
-        login.click()
-        time.sleep(1)
-
-        # Enter a valid username and password
-        username = driver.find_element(By.CSS_SELECTOR, "input#username[name='username']")
-        username.send_keys("rijulrojio")  # Replace with a valid username
-
-        password = driver.find_element(By.CSS_SELECTOR, "input#password[name='password']")
-        password.send_keys("rR@12345")  # Replace with a valid password
-
-        print('Typed username and password')
-        time.sleep(1)
-
-        # Click on the login button
-        login_button = driver.find_element(By.CSS_SELECTOR, "button.btn[type='submit']")
-        login_button.click()
-        print('Logged in')
-        time.sleep(2)
-
-if __name__ == '__main__':
-    import unittest
+if __name__ == "__main__":
     unittest.main()
